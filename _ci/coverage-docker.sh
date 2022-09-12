ce709fba621a8b43cc690166bd7d66cce4d6b3c2
Simon D. Müller <simon.mueller@h2g.ch>
2022-09-12 23:24:34 +0200

    Setup Library

---
+++ _ci/coverage-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# install and execute the coverage
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker build $script_dir/../_docker/ --tag=swiss-bill
+docker run --rm  -v=$(pwd):/application -w=/application  -e "TERM=xterm-256color"  swiss-bill  composer install
+docker run --rm  -v=$(pwd):/application -w=/application  -e "TERM=xterm-256color"  swiss-bill _ci/coverage.sh
